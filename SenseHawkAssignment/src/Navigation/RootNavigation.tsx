1953a322c5ba57403cdc80d2b169d310f4b05336
rajukumar <raju.kumaer@ei.study>
2023-11-03 11:01:13 +0530

    Navigation Component Added

---
+++ SenseHawkAssignment/src/Navigation/RootNavigation.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import LoginScreen from "../Screens/LoginScreen";
+
+
+const RootNavigation = () => {
+    const Stack = createStackNavigator();
+
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="LoginScreen"
+                screenOptions={{
+                    headerShown: false
+                }}
+            >
+                <Stack.Screen
+                    name="LoginScreen"
+                    component={LoginScreen}
+                />
+            </Stack.Navigator>
+        </NavigationContainer>
+    )
+}
+
+export default RootNavigation;
+
---
+++ SenseHawkAssignment/src/Screens/LoginScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View , Text } from 'react-native';
+
+const LoginScreen = () =>{
+    return (
+        <View>
+            <Text>This is Assignment</Text>
+        </View>
+    )
+}
+
+export default LoginScreen;
